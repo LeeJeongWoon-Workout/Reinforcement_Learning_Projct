f37463ce45463199ac83126b6abccdcfafc0ea09
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-11-17 16:31:43 +0900

    Update gridworld_value_iteration.py

--- chapter 1/gridworld_value_iteration.py
+++ chapter 1/gridworld_value_iteration.py
@@ -1,65 +1,63 @@
 import numpy as np
 
-
-ACTIONS = ('U', 'D', 'L', 'R')
-DELTA_THRESHOLD = 1e-2
-GAMMA = 0.9
-
-
+ACTIONS=('U','D',"L","R")
+DELTA_THRESHOLD=1e-1
+GAMMA=0.9
 
 class Grid:
-  def __init__(self,rows,cols):
-    self.rows=rows
-    self.cols=cols
+	def __init__(self,rows,cols):
+		self.rows=rows
+		self.cols=cols
 
+	# make field matrix
 
-  def set(self,rewards):
-    self.rewards=rewards
+	def set(self,rewards,block=None):
+		self.rewards=rewards
+		self.block=block
+	# set the rewards points
+	def set_state(self,s):
+		self.i=s[0]
+		self.j=s[1]
+	#set agent's location
+	def current_state(self):
+		return (self.i,self.j)
 
-  def set_state(self,s):
-    self.i=s[0]
-    self.j=s[1]
+	def move(self,action):
+		old_i=self.i
+		old_j=self.j
+		if action=="U":
+			self.i-=1
+		elif action=="D":
+			self.i+=1
+		elif action=="R":
+			self.j+=1
+		elif action=="L":
+			self.j-=1
 
-  def current_state(self):
-    return (self.i,self.j)
-
-
-  def move(self,action):
-    if action=="U":
-      self.i-=1
-    elif action=="D":
-      self.i+=1
-    elif action=="R":
-      self.j+=1
-    elif action=="L":
-      self.j-=1
-
-
-    if self.i<0:
-      self.i=0
-    if self.i==self.rows:
-      self.i=self.rows-1
-    if self.j<0:
-      self.j=0
-    if self.j==self.cols:
-      self.j=self.cols-1
-    return self.rewards.get((self.i,self.j),0)
-
-
-
-
-
-# 격자 공간과 각 상태에서 선택 가능한 행동을 정의
+		#if out of range then back to previous value
+		if self.i==self.rows:
+			self.i=old_i
+		if self.i<0:
+			self.i=old_i
+		if self.j==self.cols:
+			self.j=old_j
+		if self.j<0:
+			self.j=old_j
+			
+			
+# object generating function		
 def standard_grid():
-	grid = Grid(5, 5)
-	rewards = {(0, 4): 1, (3, 4): -1,(4,4):1,(3,4):-1,(1,2):1}
+  grid=Grid(3,4)
+  rewards={(0,3):1,(1,3):-1}
+  block={(1,1):-10}
+  grid.set(rewards,block)
+  return grid
 
-	grid.set(rewards)
-	return grid
 
+#print function
 def print_values(V, grid):
-	for i in range(grid.cols):
-		print("-"*grid.rows*6)
+	for i in range(grid.rows):
+		print("-----"*(grid.cols+2))
 		for j in range(grid.cols):
 			value = V.get((i, j), 0)
 			if value >= 0:
@@ -69,68 +67,52 @@ def print_values(V, grid):
 		print("")
 
 def print_policy(P, grid):
-	for i in range(grid.cols):
-		print("-"*grid.rows*6)
+	for i in range(grid.rows):
 		for j in range(grid.cols):
 			action = P.get((i, j), ' ')
 			print("  %s  |" % action, end = "")
 		print("")
 		
 		
-		
-		
-		
 
-if __name__ =='__main__':
+		
+if __name__=='__main__':
+#make object
   grid=standard_grid()
-
+  best_policy={}
   V={}
   for i in range(grid.rows):
     for j in range(grid.cols):
       point=(i,j)
-      if point in grid.rewards.keys():
-        V[point]=grid.rewards[point]
-      else:
+      if point not in grid.rewards.keys() :
         V[point]=0
+      if point in grid.rewards:
+        V[point]=grid.rewards[point]
 
-
-
+      if point in grid.block:
+        V[point]=grid.block[point]
+  print_values(V,grid)
   i=0
   while True:
     maxChange=0
     for s in V.keys():
-      if s not in grid.rewards.keys():
-        oldValue=V[s]
+      if s not in grid.rewards.keys() and s not in grid.block.keys():    
         for a in ACTIONS:
           grid.set_state(s)
-          r=grid.move(a)
-          v=r+GAMMA*V[grid.current_state()]
+          oldValue=V[s]
+          grid.move(a)
+	# at this example there is no action reward 
+          v=GAMMA*V[grid.current_state()]
           if v>oldValue:
             V[s]=v
+            best_policy[s]=a
+	# optimal bellman equation 
         maxChange=max(maxChange,np.abs(oldValue-V[s]))
-    
-    print('\n%i 번째 반복'%i,end="\n")
+        
+    print('\n%i 번째 반복'%i,end='\n')
     print_values(V,grid)
+    print("")
+    print_policy(best_policy,grid)
     i+=1
-
     if maxChange<DELTA_THRESHOLD:
-      break
-
-  best_policy={}
-  for s in V.keys():
-    if s not in grid.rewards.keys():
-      bestAction=None
-      bestValue=float('-inf')
-
-      for a in ACTIONS:
-        grid.set_state(s)
-        r=grid.move(a)
-        v=r+GAMMA*V[grid.current_state()]
-        if v>bestValue:
-          bestValue=v
-          bestAction=a
-      best_policy[s]=bestAction
-
-
-
-  print_policy(best_policy,grid)
+          break
